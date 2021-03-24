23f898b9abc43f5a16230d54bf267245285e1fd5
RobertoRamos <robram91@gmail.com>
2021-03-24 12:25:26 -0400

    seeding database

--- db/seed.js
+++ db/seed.js
@@ -1,1 +1,22 @@
 const smallBusiness = require('../models/business-model');
+const businessData = require('./sba_atx_res_open.json');
+
+// Business Data
+// const businessData = data.map(e => {
+//     const business = {};
+//     business.id = e.business_id;
+//     business.name = e.name;
+//     business.address = e.address
+
+// })
+
+smallBusiness.deleteMany({}).then(() => {
+  smallBusiness
+    .create(businessData)
+    .then(bees => {
+      console.log(bees);
+    })
+    .catch(err => {
+      console.log('Error!', err);
+    });
+});
