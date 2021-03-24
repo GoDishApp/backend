4731444f11f4e7466ca3b3118b1fda41fb813ba7
RobertoRamos <robram91@gmail.com>
2021-03-24 11:25:08 -0400

    created business model

--- models/business-model.js
+++ models/business-model.js
@@ -1,15 +1,21 @@
 const mongoose = require('../db/connection');
 const Schema = mongoose.Schema;
 
-const businessSchema = new Schema({ 
-    business_id: String,
-    name: String,
-    address: String,
-    city: String,
-    state: String,
-    postal_code: Number,
-    latitude: Number,
-    longitude: Number,
-    
+const smallBusiness = new Schema({
+  business_id: String,
+  name: String,
+  address: String,
+  city: String,
+  state: String,
+  postal_code: Number,
+  latitude: Number,
+  longitude: Number,
+  stars: Number,
+  review_count: Number,
+  is_open: Number, // could be Boolean (0 = 'no') x (1 = 'yes')
+  attributes: JSON,
+  categories: String,
+  hours: JSON,
+});
 
-}
+module.exports = mongoose.model('smallBusiness', smallBusiness);
